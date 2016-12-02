a163d08ea289383b62922b7a1685ea7c59fd073d
leon3110l <leon3110l@gmail.com>
2016-12-02 20:23:21 +0100

    animaties verandert

--- moveIt/animation/main.js
+++ moveIt/animation/main.js
@@ -1,15 +1,78 @@
+//variable declaration
+var movingDiv = document.getElementById("movingDiv");
+var ghostingDiv = document.getElementById("ghostingDiv");
+var animationDuration = 250;
+var deg = 0;
+//set everything to default values
+switchAll();
+
 function toLeft() {
-  document.getElementById("movingDiv").style.marginLeft = "0em";
-  setTimeout(ghostToLeft, 1000);
+  movingDiv.style.marginLeft = "0em";
+  switchAll();
+  setTimeout(ghostToLeft, animationDuration);
 }
 function ghostToLeft() {
-  document.getElementById("movingDiv").style.marginLeft = "0em";
+  ghostingDiv.style.marginLeft = "0em";
 }
 
 function toRight() {
-  document.getElementById("movingDiv").style.marginLeft = "10em";
-  setTimeout(ghostToRight, 1000);
+  movingDiv.style.marginLeft = "10em";
+  switchAll();
+  setTimeout(ghostToRight, animationDuration);
 }
 function ghostToRight() {
-  document.getElementById("ghostingDiv").style.marginLeft = "10em";
+  ghostingDiv.style.marginLeft = "10em";
+}
+
+function toBottom() {
+  if(movingDiv.style.marginTop != "10em") {
+    ghostingDiv.style.marginTop = "-20em";
+  }
+  movingDiv.style.marginTop = "10em";
+  switchAll();
+  setTimeout(ghostToBottom, animationDuration);
+}
+function ghostToBottom() {
+  ghostingDiv.style.marginTop = "-10em";
+}
+
+function toTop() {
+  if(movingDiv.style.marginTop != "0em") {
+    ghostingDiv.style.marginTop = "0em";
+  }
+  movingDiv.style.marginTop = "0em";
+  switchAll();
+  setTimeout(ghostToTop, animationDuration);
+}
+function ghostToTop() {
+  ghostingDiv.style.marginTop = "-10em";
+}
+
+function rotateLeft() {
+  deg -= 180;
+  movingDiv.style.transform = "rotate("+deg+"deg)";
+  ghostingDiv.style.transform = "rotate("+deg+"deg)";
+}
+
+function rotateRight() {
+  deg += 180;
+  movingDiv.style.transform = "rotate("+deg+"deg)";
+  ghostingDiv.style.transform = "rotate("+deg+"deg)";
+}
+
+function switchAll() {
+  if (movingDiv.style.marginLeft === "10em") {
+    document.getElementById("toLeft").disabled = false;
+    document.getElementById("toRight").disabled = true;
+  }else {
+    document.getElementById("toRight").disabled = false;
+    document.getElementById("toLeft").disabled = true;
+  }
+  if (movingDiv.style.marginTop === "10em") {
+    document.getElementById("toBottom").disabled = true;
+    document.getElementById("toTop").disabled = false;
+  }else {
+    document.getElementById("toTop").disabled = true;
+    document.getElementById("toBottom").disabled = false;
+  }
 }
